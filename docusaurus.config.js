ea484ec568f3a720b7b00f5d3b8fb762df96f6bb
postyizhan <185839426@qq.com>
2025-04-30 20:34:06 +0800

    fix build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'taboo.8aka.org',
+  url: 'https://taboo.8aka.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: 'TabooLib-guide',
