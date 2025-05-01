5f43351e096df40d9ac1745a7dde36d8b27e330b
千屈 <61087334+Lythrilla@users.noreply.github.com>
2025-05-01 18:10:10 +0800

    添加trmenu分类

--- src/components/KetherList/actions/trMenu.ts
+++ src/components/KetherList/actions/trMenu.ts
@@ -10,7 +10,7 @@ const trMenu: KetherActionModule = {
       description: "将动作的\{返回值\}作为\{物品特征解析式\}判断玩家是否持有。\r\\n物品特征解析式详见 \{TrMenu\} 文档。",
       provider: "TrMenu",
       type: "private",
-      category: "未分类",
+      category: "物品操作",
       syntax: "item \{action\}",
       example: "\{item\} \"material:diamond_block\"\r\\n\{item\} \"material:diamond_block,data:0,amount:64\""
     },
@@ -20,7 +20,7 @@ const trMenu: KetherActionModule = {
       description: "将动作的\{返回值\}作为菜单名称打开或关闭玩家当前菜单。",
       provider: "TrMenu",
       type: "private",
-      category: "未分类",
+      category: "菜单控制",
       syntax: "menu open \{action\}\r\\nmenu close",
       example: "\{menu open\} Example\r\\n\{menu close\}"
     },
@@ -30,7 +30,7 @@ const trMenu: KetherActionModule = {
       description: "获取或赋值玩家的\{元数据\}。\r\\n\r\\n\{meta\} 是临时存储在服务器内存,\r\\n\{data\} 则会调用 TabooLib 提供的 \{LocalPlayer\} 存储在玩家独立数据文件。",
       provider: "TrMenu",
       type: "private",
-      category: "未分类",
+      category: "数据存储",
       syntax: "data (get|set|del) \{action\} \[to \{action\}\]\r\\nmeta (get|set|del) \{action\} \[to \{action\}\]",
       example: "\{data\} \{set\} test \{to\} value\r\\n\{data\} \{del\} test\r\\n\r\\n\{meta\} \{set\} test \{to\} value\r\\n\{meta\} \{del\} test\r\\n\r\\nprint \{data\} \{get\} test"
     },
@@ -40,7 +40,7 @@ const trMenu: KetherActionModule = {
       description: "将动作的\{返回值\}作为 \{Vault\} 货币判断玩家是否持有。",
       provider: "TrMenu",
       type: "private",
-      category: "未分类",
+      category: "经济系统",
       syntax: "money \{action\}",
       example: "print \{money\} 100"
     },
@@ -50,7 +50,7 @@ const trMenu: KetherActionModule = {
       description: "将动作的\{返回值\}作为 \{PlayerPoints\} 货币判断玩家是否持有。",
       provider: "TrMenu",
       type: "private",
-      category: "未分类",
+      category: "经济系统",
       syntax: "points \{action\}",
       example: "print \{points\} 100"
     },
@@ -60,7 +60,7 @@ const trMenu: KetherActionModule = {
       description: "替换动作的\{返回值\}中所有 \{TrMenu\} 变量函数。 \r\\n\r\\n引用：\r\\nhttps://trmenu.trixey.cc/usage/functions",
       provider: "TrMenu",
       type: "private",
-      category: "未分类",
+      category: "变量处理",
       syntax: "var\[s\] \{action\}",
       example: "tell \{var\} \"\{\\\{\}<Index>\{\\\}\}\"\r\\ntell \{var\} \"\{\\\{\}meta: <Key>\{\\\}\}\""
     },
